66cb3d7c9db2f5428d27b5658f94c86b622f99b8
shoebe <smarleau.2019@gmail.com>
2024-02-17 22:08:03 -0500

    add tl to center offset converter

--- src/output.rs
+++ src/output.rs
@@ -90,3 +90,48 @@ impl AnimationSet {
         })
     }
 }
+
+pub fn tl_offset_to_centered(tl_offset: (u32, u32), sprite_size: (u32, u32), canvas_size: (u32, u32)) -> (f32, f32){
+    // Motivation: do not want the 'centered' sprite to be offset by 1/2 a pixel compared to other sprites
+    //             This happens if the canvas has even dimensions, the resulting sprite will be too centered
+
+    // Ex Sprite: (x, y, w, h) = (3, 3, 13, 13)
+    // canvas: 17x17
+    // canvas center= (8.5,8.5)
+    // center of the sprite: (13,13)/2 + (3,3) = (9.5, 9.5)
+    // center offset = (8.5,8.5) - (9.5,9.5) = (-1,-1)
+    // center offset in action = (-1,-1) - (13,13)/2
+    //                         = (-7.5,-7.5) half a pixel off
+
+    // THEREFORE: add a (+1,+1) offset to canvas center when canvas size is odd
+
+    let canvas_center = ((canvas_size.0 + canvas_size.0 % 2) as f32 / 2.0, (canvas_size.1 + canvas_size.1 % 2) as f32 / 2.0);
+
+    let local_sprite_center = (sprite_size.0 as f32 / 2.0,sprite_size.1 as f32 / 2.0);
+
+    let sprite_center_wrt_canvas = (tl_offset.0 as f32 + local_sprite_center.0 ,tl_offset.1 as f32 + local_sprite_center.1);
+
+    (canvas_center.0 - sprite_center_wrt_canvas.0, canvas_center.1 - sprite_center_wrt_canvas.1)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::output::tl_offset_to_centered;
+
+    #[test]
+    fn it_has_no_half_pixel_vertices() {
+        for (c_w, c_h, s_w, s_h) in [
+            (22, 22, 16, 16), // even even
+            (22, 22, 17, 17), // even odd
+            (21, 21, 17, 17), // odd odd
+            (21, 21, 16, 16), // odd even
+        ] {
+            let off = tl_offset_to_centered((0,0), (s_w, s_h), (c_w, c_h));
+            let left_top_pos = (off.0 + s_w as f32/2.0, off.1 + s_h as f32 / 2.0);
+            
+            // should never have 0.5 offsets, no matter the even/odd combo of sprite/canvas
+            assert!(left_top_pos.0.fract() < 0.00001);
+            assert!(left_top_pos.1.fract() < 0.00001);
+        }
+    }
+}
